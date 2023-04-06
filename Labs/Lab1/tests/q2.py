d4475ee5c9e01912791cf13512fc3ca40bb4e598
Laura Baracaldo <LauraBaracaldo@users.noreply.github.com>
2023-04-05 22:19:01 -0700

    Adding Labs

---
+++ Labs/Lab1/tests/q2.py
@@ -0,0 +1,10 @@
+OK_FORMAT = True
+test = {   'name': 'q2',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(y, int)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> 0 < y < 100\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> print(y)\n2\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ Labs/Lab1/tests/q3.py
@@ -0,0 +1,8 @@
+OK_FORMAT = True
+test = {   'name': 'q3',
+    'points': 3,
+    'suites': [   {   'cases': [{'code': '>>> isinstance(z, int)\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> 0 < z < 100\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
